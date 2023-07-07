fc2ff387045298758189442cea5609b2be6daaed
-Lunat1c- <arturgrabowski100@onet.pl>
2023-07-07 20:09:37 +0200

    Collectible in-world animation

--- Assets/Scripts/PrefabsScripts/Collectible.cs
+++ Assets/Scripts/PrefabsScripts/Collectible.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
+    public float rotationSpeed = 90f;
+    public float amplitude = 0.25f;
+    public float frequency = 6f;
+    public float speed = 30f;
+
+    private Vector3 startPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+
+        float yOffset = amplitude * Mathf.Sin(Time.time * frequency);
+        float movementSpeed = speed * Time.deltaTime;
+
+        Vector3 newPosition = startPosition + new Vector3(0f, yOffset, 0f);
+        transform.position = Vector3.Lerp(transform.position, newPosition, movementSpeed);
     }
 
     void OnTriggerEnter(Collider other)
